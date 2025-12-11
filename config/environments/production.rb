7ea1f6032de65849fd8919fc45d19717353cc870
Ahmad <Ahmadmajid1994@gmail.com>
2025-12-11 09:36:03 +0000

    Add db:migrate to Render build command

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_storage.variant_processor = :vips
 
   config.log_tags = [ :request_id ]
   config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
